2edcf81544578bfad6974a03efd0c5e2c132b7af
Alvin Hsia <alvinhsia@gmail.com>
2016-02-24 20:12:18 -0800

    Initial Commit

---
+++ HW 4Tests/HW_4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HW_4Tests.swift
+//  HW 4Tests
+//
+//  Created by Alvin Hsia on 2/24/16.
+//  Copyright © 2016 Alvin Hsia. All rights reserved.
+//
+
+import XCTest
+@testable import HW_4
+
+class HW_4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
